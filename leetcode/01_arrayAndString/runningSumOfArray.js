296f06627152e62f0e7c3453b2f2883303e6231a
Mia Dinh <miadinh38@gmail.com>
2024-07-31 21:25:38 -0400

    Add another solution

--- leetcode/01_arrayAndString/runningSumOfArray.js
+++ leetcode/01_arrayAndString/runningSumOfArray.js
@@ -39,3 +39,6 @@ var runningSum = function(nums) {
 console.log(runningSum([1,2,3,4])); // [ 1, 3, 6, 10 ]
 console.log(runningSum([1,1,1,1,1])); // [ 1, 2, 3, 4, 5 ]
 console.log(runningSum([3,1,2,10,1])); // [ 3, 4, 6, 16, 17 ]
+
+// SOLUTION I LIKE:
+// var runningSum = ((nums, val = 0) => nums.map(x => val = val + x));
